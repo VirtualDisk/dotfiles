524a22a046237c208a3040ce5cb929ec350064e4
VirtualDisk <zoe.blanco1911@gmail.com>
2020-12-01 10:34:22 -0500

    remove vim from mackup, change vim install logic, install oh-my-zsh unattended to stop shell interrupt

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ set_vars()  {
     export HOMEBREW_BUNDLE_FILE="$HOME/.setup/Brewfile"
     export TERMINAL="iterm2"
 	GITDIR="$HOME/src"
+    export BASE_URL=${BASE_URL:-"https://raw.githubusercontent.com/Mayccoll/Gogh/master"}
+
 }
 
 brewtime()  {
@@ -17,22 +19,60 @@ brewtime()  {
 
 oh_my_zsh() {
     echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
 
 gogh_install()  {
-    mkdir -p "$GITDIR"
-    echo "Cloning Gogh repo..."
-    git clone https://github.com/Mayccoll/Gogh.git "$GITDIR"
-    echo "Installing brogrammer theme. Welcome, bro."
-    "$GITDIR"/Gogh/themes/brogrammer.sh
+    export COLOR_01="#1f1f1f"           # HOST
+    export COLOR_02="#f81118"           # SYNTAX_STRING
+    export COLOR_03="#2dc55e"           # COMMAND
+    export COLOR_04="#ecba0f"           # COMMAND_COLOR2
+    export COLOR_05="#2a84d2"           # PATH
+    export COLOR_06="#4e5ab7"           # SYNTAX_VAR
+    export COLOR_07="#1081d6"           # PROMP
+    export COLOR_08="#d6dbe5"           #
+
+    export COLOR_09="#d6dbe5"           #
+    export COLOR_10="#de352e"           # COMMAND_ERROR
+    export COLOR_11="#1dd361"           # EXEC
+    export COLOR_12="#f3bd09"           #
+    export COLOR_13="#1081d6"           # FOLDER
+    export COLOR_14="#5350b9"           #
+    export COLOR_15="#0f7ddb"           #
+    export COLOR_16="#ffffff"           #
+
+    export BACKGROUND_COLOR="#131313"   # Background Color
+    export FOREGROUND_COLOR="#d6dbe5"   # Text
+    export CURSOR_COLOR="$FOREGROUND_COLOR" # Cursor
+    export PROFILE_NAME="Brogrammer"
+    # =============================================================== #
+    # | Apply Colors
+    # ===============================================================|#
+    SCRIPT_PATH="${SCRIPT_PATH:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}"
+    PARENT_PATH="$(dirname "${SCRIPT_PATH}")"
+
+    # Allow developer to change url to forked url for easier testing
+    # IMPORTANT: Make sure you export this variable if your main shell is not bash
+    BASE_URL=${BASE_URL:-"https://raw.githubusercontent.com/Mayccoll/Gogh/master"}
+
+    if [[ -e "${PARENT_PATH}/apply-colors.sh"  ]]; then
+          bash "${PARENT_PATH}/apply-colors.sh"
+      else
+            if [[ "$(uname)" = "Darwin"  ]]; then
+                    # OSX ships with curl and ancient bash
+                    bash -c "$(curl -so- "${BASE_URL}/apply-colors.sh")"
+                    else
+                    # Linux ships with wget
+                    bash -c "$(wget -qO- "${BASE_URL}/apply-colors.sh")"
+            fi
+    fi
+
 }
 
 vim_install()   {
     echo "Installing vim configs..."
 
-    (mkdir ~/.vim_runtime
-    cd ~/.vim_runtime
+    (cp "$SCRIPTDIR/.vim_runtime $HOME"
 
     echo 'set runtimepath+=~/.vim_runtime
 
@@ -51,7 +91,7 @@ vim_install()   {
 
 vim_tf()    {
     echo "Installing vim terraform plugin..."
-    (cd ~/.vim_runtime/my_plugins;
+    (cd "$HOME/.vim_runtime/my_plugins";
     git clone https://github.com/hashivim/vim-terraform.git)
 }
 
@@ -92,5 +132,7 @@ main()  {
     vim_install
     vim_tf
     aliases
+    darkmode
+    macktruck
 }
 main
