0f687f097948cc018d5142ca1868b0e0901472cd
Zoe Blanco <zoe.blanco@c02f248yq05f.lan>
2021-03-15 09:55:37 -0400

    Symlink vim_runtime && .zshrc, remove alias writing
    Committer: Zoë A. Blanco <zoe.blanco1911@gmail.com>

--- setup.sh
+++ setup.sh
@@ -38,48 +38,13 @@ git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" \
 # sed -i -e "s/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/g" "$HOME/.zshrc"
 }
 
-vim_install()   {
-    echo "Installing vim configs..."
-
-    cp -R ".vim_runtime" "$HOME"
-    #cp -R "$PWD/.vim_runtime" "$HOME"
-
-    echo 'set runtimepath+=~/.vim_runtime
-
-    source ~/.vim_runtime/vimrcs/basic.vim
-    source ~/.vim_runtime/vimrcs/filetypes.vim
-    source ~/.vim_runtime/vimrcs/plugins_config.vim
-    source ~/.vim_runtime/vimrcs/extended.vim
-
-    try
-    source ~/.vim_runtime/my_configs.vim
-    catch
-    endtry' > ~/.vimrc
-
-    echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+vimstall() {
+    ln -sf $(PWD)/.vim_runtime $(HOME)/.vim_runtime
 }
 
-vim_tf()    {
-    echo "Installing vim terraform plugin..."
-    (cd "$HOME/.vim_runtime/my_plugins";
-    git clone https://github.com/hashivim/vim-terraform.git)
+zshstall() {
+    ln -sf $(PWD)/.zshrc $(HOME)/.zshrc
 }
-
-aliases()     {
-    #TODO: this doesn't work, and likely needs to be a heredoc
-   echo "# Aliases
-    alias k=kubectl
-    alias mk=minikube
-    alias tf=terraform
-    alias td=terraform-docs
-    alias gam="$HOME/bin/gamadv-xtd3/gam"
-    alias y="yes > /dev/null"
-    alias cat="pygmentize -g"
-    alias grep=ag
-    alias pj="cd ~/Projects"
-    alias it="cd ~/Projects/IT-SaaS-automation"
-    alias inf="cd ~/Projects/infrastructure"" >> ~/.zshrc
-    }
 
 set_defaults()      {
     echo "Enabling dark mode..."
@@ -100,8 +65,6 @@ main()  {
     brewtime
     oh_my_zsh
     vim_install
-    vim_tf
-    #aliases
     set_defaults
     macktruck
     plevel10k
