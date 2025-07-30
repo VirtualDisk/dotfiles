6fe7af80c6fc12234cb7c2e68937cbbfcfcaf504
VirtualDisk <zoe@zoeblan.co>
2025-07-30 14:00:16 -0400

    add json schemas

--- concerns/vim/starter/lua/plugins/nvim-lspconfig.lua
+++ concerns/vim/starter/lua/plugins/nvim-lspconfig.lua
@@ -6,6 +6,21 @@ return {
         terraformls = {},
         dockerls = {},
         docker_compose_language_service = {},
+        jsonls = {
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+          end,
+          settings = {
+            json = {
+              format = {
+                enable = true,
+              },
+              validate = { enable = true },
+            },
+          },
+        },
         yamlls = {
           -- Have to add this for yamlls to understand that we support line folding
           capabilities = {
